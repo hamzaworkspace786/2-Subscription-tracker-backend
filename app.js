f3c33b583cb8621817be496a74fc74b2592f6a48
Hamza <hamzasworkspace1@gmail.com>
2026-02-11 11:40:18 +0500

    Handling Bots adn Rate Limit

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ import { PORT } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjectMiddleware from "./middlewares/arcject.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
+app.use(arcjectMiddleware)
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
